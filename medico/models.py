61790144a71676a8b5d9938fa47dbb499bf26d99
amarocesar <amaro.txt@gmail.com>
2020-03-09 21:37:05 -0300

    criado start app criado models, iniciando projecto

---
+++ medico/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Medico(models.Model):
+    nome = models.CharField(max_length=120)
+    crm = models.CharField(max_length=40)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    especialidade = models.ForeignKey(
+        'especialidade.Especialidade',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
