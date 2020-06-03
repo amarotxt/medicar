0ff47dc850b510811c3a979dcb87689684c25e42
amarocesar <amaro.txt@gmail.com>
2020-06-02 23:22:56 -0300

    urls para jwt e usuario

--- source/medicar/urls.py
+++ source/medicar/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
 from .router import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('accounts/', include('allauth.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('usuario/', include('usuario.urls')),
+    path('token-auth/', obtain_jwt_token)
 ]
