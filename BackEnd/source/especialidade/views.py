b19c856be3048d9be67a0c0c4228ea9b72b9f761
amarocesar <amaro.txt@gmail.com>
2020-06-06 15:58:53 -0300

    adicionando frontEnd em andamento

---
+++ BackEnd/source/especialidade/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from especialidade.models import Especialidade
+from especialidade.serializers import EspecialidadeSerializer
+# Create your views here.
+from rest_framework import filters
+    
+
+class EspecialidadeViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Especialidade.objects.all()
+    serializer_class = EspecialidadeSerializer
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nome']
---
+++ BackEnd/source/medico/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import filters
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from medico.models import Medico
+from medico.serializers import MedicoSerializer
+
+
+# Create your views here.
+
+class MedicoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Medico.objects.all()
+    serializer_class = MedicoSerializer
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, )
+    search_fields = ['nome']
+    filterset_fields = ['especialidade']
