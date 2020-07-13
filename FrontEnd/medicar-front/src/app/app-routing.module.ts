272dc64538fae145b5182246fe9ab3e13f431b53
amarocesar <amaro.txt@gmail.com>
2020-07-13 10:24:36 -0300

    removendo components nao mais utilizados

--- FrontEnd/medicar-front/src/app/app-routing.module.ts
+++ FrontEnd/medicar-front/src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NovaConsultaComponent } from './nova-consulta/nova-consulta.component';
-import { AgendaComponent } from './components/agenda/agenda.component';
-import { MedicosComponent } from './components/medicos/medicos.component';
-import { EspecialidadesComponent } from './components/especialidades/especialidades.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
 import { LoginComponent } from './components/user/login/login.component';
 import { AuthGuard } from './components/auth/auth.guard';
@@ -13,7 +9,6 @@ import { ConsultasComponent } from './consultas/consultas.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent,canActivate:[AuthGuard] },
   {
     path: 'signup', component: UserComponent,
     children: [{ path: '', component: SingUpComponent }]
@@ -23,12 +18,8 @@ const routes: Routes = [
     children: [{ path: '', component: LoginComponent }]
   },
   { path : '', redirectTo:'/login', pathMatch : 'full'},
-  {path: 'list-especialidades', component: EspecialidadesComponent},
   {path: 'list-consultas', component: ConsultasComponent},
   {path: 'consulta', component: NovaConsultaComponent},
-  
-  {path: 'list-medicos/:especialidadeId', component: MedicosComponent},
-  {path: 'list-agendas/:medicoId', component: AgendaComponent},
 ];
 
 @NgModule({
--- FrontEnd/medicar-front/src/app/app.module.ts
+++ FrontEnd/medicar-front/src/app/app.module.ts
@@ -7,12 +7,8 @@ import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MedicosComponent } from './components/medicos/medicos.component';
-import { EspecialidadesComponent } from './components/especialidades/especialidades.component';
-import { AgendaComponent } from './components/agenda/agenda.component';
 import { UserComponent } from './components/user/user.component';
 import { LoginComponent } from './components/user/login/login.component';
-import { HomeComponent } from './components/home/home.component';
 import { SingUpComponent } from './components/user/sing-up/sing-up.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserService } from './components/user/shared/user.service';
@@ -26,13 +22,9 @@ import { NovaConsultaComponent } from './nova-consulta/nova-consulta.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MedicosComponent,
-    EspecialidadesComponent,
-    AgendaComponent,
     UserComponent,
     LoginComponent,
     SingUpComponent,
-    HomeComponent,
     ConsultasComponent,
     NovaConsultaComponent,
   ],
