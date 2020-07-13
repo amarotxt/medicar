b2f37475b1a27546dfc575517334959cb52c39bc
amarocesar <amaro.txt@gmail.com>
2020-07-13 15:12:42 -0300

    adicionando campos para lembrar usuario

--- FrontEnd/medicar-front/src/app/components/user/login/login.component.ts
+++ FrontEnd/medicar-front/src/app/components/user/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { UserService } from '../shared/user.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
@@ -10,24 +11,42 @@ import { UserService } from '../shared/user.service';
 })
 export class LoginComponent implements OnInit{
   isLoginError : boolean = false;
-  constructor(private userService : UserService,private router : Router) { }
+  rememberMe : boolean =false
+  public formData:any = {};
+  constructor(private userService : UserService,
+    private router : Router,
+    private _cookie: CookieService) {
+    if(!!_cookie.get('remember') && _cookie.get('remember') != "false"){   
+        console.log("verificar",_cookie.get('remember'))
+        this.formData["userName"] = _cookie.get('username')
+        this.formData["password"] = _cookie.get('password')
+    }        
 
-  ngOnInit() {
   }
 
-  OnSubmit(userName,password){
+  ngOnInit() {
+
+  }
+
+  OnSubmit(userName,password, remember){
+    if (remember){
+      console.log("registerRemember")
+      this._cookie.set('username',userName);
+      this._cookie.set('password',password);
+    }
+    this._cookie.set('remember',remember);
     this.userService.userAuthentication(userName,password).subscribe(data => {
-      
-      console.log(this.userService.usuario)
       localStorage.setItem('userToken',data["token"]);
       localStorage.setItem('userId',data["user_id"]);
       this.router.navigate(['/list-consultas']);
     }, error => {
+      this.isLoginError = true;
       console.error(error);
       }
     );
     
   }
+ 
 }
 
 
